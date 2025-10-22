45bd7157e741ff51699515c1c8130be772bc6314
Your Name <you@example.com>
2025-10-23 02:46:36 +0530

    Initial commit

---
+++ prob1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{int i;
+    int a;
+    printf("enter the number you want table of");
+    scanf("%d",&a);
+    for(i= 1; i <= 10; i++)
+  {
+    printf("%d x %d = %d\n", a,i, a * i); }
+    return 0;}
+    
