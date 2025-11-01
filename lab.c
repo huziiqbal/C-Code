45f0cfd8d9733882742e4d66eb897dcad3c30303
Your Name <you@example.com>
2025-11-01 20:02:21 +0530

    new basic ques added

---
+++ lab.c
@@ -0,0 +1,80 @@
+// // #the sum off two numbers
+// void main (){
+//         int  a, b , sum ;
+//         printf("Enter the first number ");
+//         scanf("%d" , &a);
+//         printf("enter the second number ");
+//         scanf("%d",&b);
+//         // printf("the sum of the number is %d" , a+b);
+//         sum=a+b;
+//         printf("the sum of %d and %d is %d ", a,b,sum);
+//     }
+//     // swapping of numbers
+
+//     // int a=5,b=12;
+//     // int c = a;
+//     // a=b;
+//     // b=c;
+//     // printf("the value of a after swapping is %d " )
+
+// #include <stdio.h>
+// int main(){
+//     int a , b ;
+//     printf("Enter the value of a ");
+//     scanf("%d",&a);
+//     printf("enter the the value of b ");
+//     scanf("%d", &b);
+//     int c = a;
+//     a=b;
+//     printf("The value of a after swapping i %d , and the value if b after swapping is %d",b,c);
+// }
+// // swapping of number
+// // int a=5,b=12;
+// // int c=a;
+// // a=b;
+// // b=c;
+// // #include <stdio.h>
+// // int main(){
+// //     int a, b;
+// //     printf("enter the value of a");
+// //     scanf('%d',)
+
+// #include <stdio.h>
+// void main (){
+//     int a,b ;
+//     int temp = a , temp2 = b ;
+//     printf("enter the first temporary number ");
+//     scanf("%d" , &temp);
+//     printf("enter the second temporary variable ");
+//     scanf("%d", &temp2);
+//     a = temp2;
+//     b= temp;
+//     printf("the value of A = %d \nand the value of b = %d\n" , temp , temp2);
+//     printf("The value of a after swapping is = %d \n and teh value of ba after swapping is %d" , a,b);
+// }
+// #include <stdio.h>
+// void main(){
+//     int a,b;
+// printf("Enter the first number");
+// scanf("%d", &a);
+// printf("Enter te second number ");
+// scanf("%d",&b);
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// printf("the value of a is %d and the value of b is %d " , a,b);
+// }
+// int main(){
+//     int a, b;
+//     printf('enter theb value a');
+
+
+#include <stdio.h>
+void main(){
+    char k[] = "huzaifa";
+    int s = 7878;
+    printf("the size of name is %d ",sizeof(k));
+    printf("the size of integer is %d", sizeof(s));
+
+}
+
