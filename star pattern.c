4cf798013977d7ea68355ac49e1746d6e6e23399
Your Name <huziiqbal00@gmail.com>
2025-11-28 02:42:02 +0530

    star pattern of pyramid

--- star pattern.c
+++ star pattern.c
@@ -12,4 +12,27 @@ printf("%c", array[h]);
 }
 printf("\n");
 }}
+// pattern 2
+//date = 28/11/2025
+#include<stdio.h>
+void main()
+{
+int a,i,n,h,j,g,k,spaces,stars;
+// char ch = ' ';
+printf("Enter the number of rows of star pyramid you want to print");
+scanf("%d",&a);
+char array[a];
+for (i=0;i<a;i++){
+array[i]= '*';}
+for (n=1;n<=a;n++){
+    spaces = a-n;
+    stars=2*n - 1;
+for (h=spaces;h>=0;h--){
+        printf(" ");}
+for (k=stars;k>=1;k--){
+            printf("%c",array[0]);}
 
+    printf("\n");
+}
+}
+  
