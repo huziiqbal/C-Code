8b11e04e2bffe9e99c6be548f81f7d786617f503
Your Name <huziiqbal00@gmail.com>
2025-12-19 03:23:03 +0530

    fibonacci series through pointers

--- pointer_questions.c
+++ pointer_questions.c
@@ -1,15 +1,38 @@
-#include<stdio.h>
-int fact(int *p){
-    if (*p==0 || *p ==1){
+// #include<stdio.h>
+// int fact(int *p){
+//     if (*p==0 || *p ==1){
+//         return 1;
+//     }
+//     int a = *p -1;
+//     return *p  * fact(&a);
+// }
+// int main (){
+//     int num;
+//     printf("Enter the number you want the factorial of:");
+//     scanf("%d",&num);
+//     int *ptr =&num;
+//     printf("The factorial of %d is %d",*ptr,fact(&num));
+// }
+int fib(int *p){
+    if (*p == 0 ){
+        return 0;
+    }
+    else if (*p ==1){
         return 1;
     }
-    int a = *p -1;
-    return *p  * fact(&a);
+    else {
+        int ones = *p-1;
+        int twos = *p-2;
+        return fib(&ones)+fib(&twos);
+    }
 }
-int main (){
+int main(){
     int num;
-    printf("Enter the number you want the factorial of:");
+    printf("Enter the value of number upto which you want to print the fibonacci series: ");
     scanf("%d",&num);
-    int *ptr =&num;
-    printf("The factorial of %d is %d",*ptr,fact(&num));
+    printf("The fibonacci till series %d is:",num);
+    for (int i=0;i<=num;i++){
+    printf(" %d",fib(&i));
+    }
 }
+
