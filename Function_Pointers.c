54afbfa0d32f22386a1a8174d8be57006b6f5a03
Your Name <huziiqbal00@gmail.com>
2026-03-15 21:51:06 +0530

    array frequency using dynamic memory allocation

---
+++ Function_Pointers.c
@@ -0,0 +1,22 @@
+// // Simple add/sub operation using fxn pointer array//
+// #include <stdio.h>
+// int add(int n, int m){
+//     return n + m;
+// }
+// int sub(int n, int m){
+//     return n - m;
+// }
+// int main()
+// {
+//     int a , b;
+//     int choice;
+//     printf("Enter 2 numbers for operation: ");
+//     scanf("%d %d", &a,&b);
+//     printf("Enter your choice for operation:\n1.add\n2.sub\n");
+//     scanf("%d",&choice);
+//     int (*ptr[2])() = {add,sub};
+//     int result = ptr[choice-1](a,b);
+//     printf("Result = %d", result);
+
+// }
+
--- unsolved.c
+++ unsolved.c
@@ -23,3 +23,33 @@ int main()
 		}
 	}
 }
+					// OR //
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int arr[] = {1,1,2,3,3,4,8,8,8,16};
+    int max = 0 ;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for (int i =0 ; i< n ; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    int * visited = {0};
+    visited=(int *)calloc(max,sizeof(int));
+    //  int visited[max + 1];
+    //   for (int i = 0; i <= max; i++){
+    //     visited[i] = 0;
+    // }
+    for ( int i = 0 ; i < n ; i++){
+                visited[arr[i]]++;
+
+    }
+    for (int i=0;i<=max;i++){
+        if (visited[i] != 0 ){
+            printf("%d : %d\n", i , visited[i]);
+        }
+    }
+
+}
