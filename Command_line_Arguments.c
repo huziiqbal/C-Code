89964da04be259107dc3eb2c194f151177cc5137
Your Name <huziiqbal00@gmail.com>
2026-03-22 22:37:44 +0530

    matrix and array questions

--- Command_line_Arguments.c
+++ Command_line_Arguments.c
@@ -1,72 +1,72 @@
-// //Q1.
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// int main(int argc, char const *argv[])
-// {
-//     int a = atoi(argv[2]);
-//     int b = atoi(argv[3]);
+//Q1.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    int a = atoi(argv[2]);
+    int b = atoi(argv[3]);
 
-//     if (strcmp(argv[1],"sum") == 0){
-//         printf("Sum = %d\n", a+b);
-//     }
-//     else if (strcmp(argv[1],"subtract") == 0){
-//         printf("subtract = %d", a-b);
-//     }
-//     else if (strcmp(argv[1],"multiply") == 0){
-//         printf("multiply = %d", a*b);
-//     }
-//     else if (strcmp(argv[1],"divide") == 0){
-//         printf("divide = %d", a/b);
-//     }
-//     return 0;
-// }
-// /*gcc Command_line_Arguments.c -o Command_line_Arguments*/
+    if (strcmp(argv[1],"sum") == 0){
+        printf("Sum = %d\n", a+b);
+    }
+    else if (strcmp(argv[1],"subtract") == 0){
+        printf("subtract = %d", a-b);
+    }
+    else if (strcmp(argv[1],"multiply") == 0){
+        printf("multiply = %d", a*b);
+    }
+    else if (strcmp(argv[1],"divide") == 0){
+        printf("divide = %d", a/b);
+    }
+    return 0;
+}
+/*gcc Command_line_Arguments.c -o Command_line_Arguments*/
 
 
-// // Q 2.
-// #include <stdio.h>
-// #include <stdlib.h>
-// int main(int argc, char const *argv[])
-// {
+// Q 2.
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
 
-//     // int largest = 0 ;
-//     int largest = atoi(argv[1]);
-//     int n = argc;
-//     printf("The size of argv is %d \n" , n);
-//     for (int i = 1 ; i < n ; i++){
-//         int j = atoi(argv[i]);
-//          if (j > largest){
-//             largest = j;
-//          }
-//     }
-//     printf("The largest element is %d", largest);
+    // int largest = 0 ;
+    int largest = atoi(argv[1]);
+    int n = argc;
+    printf("The size of argv is %d \n" , n);
+    for (int i = 1 ; i < n ; i++){
+        int j = atoi(argv[i]);
+         if (j > largest){
+            largest = j;
+         }
+    }
+    printf("The largest element is %d", largest);
 
-// }
+}
 
-// // Q.3
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// int main(int argc, char const *argv[])
-// {
-//     for (int i=argc - 1 ; i>0 ; i--){
-//         printf("%s ",argv[i]);
-//     }
-// }
-// //  Q4. factorial;
-// #include <stdio.h>
-// #include <stdlib.h>
-// int main(int argc, char const *argv[])
-// {
+// Q.3
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    for (int i=argc - 1 ; i>0 ; i--){
+        printf("%s ",argv[i]);
+    }
+}
+//  Q4. factorial;
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
 
-//     int factorial = atoi(argv[1]);
-//     for (int i = factorial-1 ; i>0 ;i--){
-//         factorial = factorial * i ;
-//     }
+    int factorial = atoi(argv[1]);
+    for (int i = factorial-1 ; i>0 ;i--){
+        factorial = factorial * i ;
+    }
 
-//     printf("The factoriala is %d", factorial);
-// }
+    printf("The factoriala is %d", factorial);
+}
 
 #include <stdio.h>
 #include<stdlib.h>
--- sparse_matrix.c
+++ sparse_matrix.c
@@ -112,3 +112,226 @@ int main (){
      }
 
 }
+
+
+// A → Stores the nonzero values in row-wise order.
+// IA → Stores the starting index of each row’s nonzero values in A. The last index in IA is the total number of nonzero elements.
+// JA → Stores the column index of each nonzero element in A.
+
+// Input 1 :              Output 1 :
+// 4 5
+// 0 0 0 0 1             0 0 0 0 1
+// 5 8 0 0 0             5 8 0 0 0
+// 0 0 3 0 0             0 0 3 0 0
+// 0 6 0 0 1             0 6 0 0 1
+//                      A = [ 1 5 8 3 6 1 ]
+//                      IA = [ 0 1 3 4 6 ]
+//                      JA = [ 4 0 1 2 1 4 ]
+
+
+
+#include<stdio.h>
+int main (){
+
+    int r , c;
+    scanf("%d %d",&r,&c);
+    int matrix[r][c];
+    for ( int i = 0 ; i < r ; i++){
+        for ( int j = 0 ; j < c ; j++){
+            scanf("%d" , &matrix[i][j]);
+        }
+    }
+    int A[10];
+    int IA[r+1];
+    int JA[10];
+    int k = 0 ;
+    int count = 0 ;
+    for ( int i = 0 ; i < r ; i++){
+        for ( int j = 0 ; j < c ; j++){
+            if ( matrix[i][j] != 0){
+                A[k] = matrix[i][j];
+                count++;
+                k++;
+            }
+        }
+    }
+    for ( int i = 0 ; i < r ; i++){
+        for ( int j = 0 ; j < c ; j++){
+            printf("%d " ,matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("A = [ ");
+    for ( int i = 0 ; i<count ; i++ ){
+        printf("%d ",A[i]);
+    }
+    printf("]\n");
+    IA[0] = 0;
+    int m = 1;
+    int count3;
+    for ( int i = 0 ; i < r ; i++){
+        count3 = 0;
+        for ( int j = 0 ; j < c ; j++){
+            if ( matrix[i][j] != 0){
+                count3++;
+            }
+        }
+            IA[m] = IA[m-1] + count3;
+            m++;
+    }
+    printf("IA = [ ");
+    for ( int i = 0 ; i<r+1 ; i++ ){
+        printf("%d ",IA[i]);
+    }
+    printf("]\n");
+
+
+    int l = 0;
+    int count2 = 0;
+    for ( int i = 0 ; i < r ; i++){
+        for ( int j = 0 ; j < c ; j++){
+            if ( matrix[i][j] != 0){
+                JA[l] = j;
+                count2++;
+                l++;
+            }
+        }
+    }
+
+    printf("JA = [ ");
+    for ( int i = 0 ; i<count2 ; i++ ){
+        printf("%d ",JA[i]);
+    }
+    printf("]");
+}
+
+// Input 1 :
+// 5 12 3
+// 101 3 25
+// 102 4 30
+// 101 5 28
+// 101
+
+// Output 1 :
+// 101 3 25
+// 102 4 30
+// 101 5 28
+// Average Reading: 27.67
+// Sensor 101: 25
+
+
+#include<stdio.h>
+int main (){
+    int  n , m ,k;
+    scanf("%d %d %d",&n,&m,&k);
+    int matrix[k][3];
+    for (int j = 0 ; j < k ; j++ ){
+        for ( int i = 0 ; i < 3 ;i++){
+            scanf("%d",&matrix[j][i]);
+        }
+    }
+
+    float read_avg = 0.0;
+    int sensor_id;
+    scanf("%d",&sensor_id);
+    for ( int j = 0 ; j < k ; j++){
+        for (int i = 0 ;i < 3 ; i++){
+            printf("%d",matrix[j][i]);
+            if ( i == 2 ){
+                read_avg = read_avg + matrix[j][i];
+            }
+        }
+        printf("\n");
+    }
+
+    float Avg_reading = read_avg /k;
+    printf("Average Reading: %.2f\n",Avg_reading);
+    int found = 0 ;
+
+    for (int i = 0 ;i < k ; i++){
+        if ( matrix[i][0] == sensor_id){
+             printf("Sensor %d: %d\n",matrix[i][0],matrix[i][2]);
+             found = 1;
+             break;
+        }
+    }
+    if (found==0){
+         printf("No data recorded for this sensor.\n");
+    }
+    return 0 ;
+}
+
+// Input 1 :
+// 3 4
+// 5
+// 1 0 10
+// 1 2 20
+// 2 0 30
+// 3 3 40
+// 2 2 50
+// Output 1 :
+// Sparse Array (row_index col_index quantity):
+// 1 0 10
+// 1 2 20
+// 2 0 30
+// 3 3 40
+// 2 2 50
+
+// Total quantity ordered per product:
+// Product 0: 40
+// Product 2: 70
+// Product 3: 40
+
+// Input 2 :
+// 3 4
+// 5
+// 0 0 10
+// 0 1 20
+// 1 0 30
+// 2 2 15
+// 1 3 5
+// Output 2 :
+// Sparse Array (row_index col_index quantity):
+// 0 0 10
+// 0 1 20
+// 1 0 30
+// 2 2 15
+// 1 3 5
+
+// Total quantity ordered per product:
+// Product 0: 40
+// Product 1: 20
+// Product 2: 15
+// Product 3: 5
+
+#include<stdio.h>
+int main(){
+    int n ,m , k ;
+    scanf("%d %d",&n,&m);
+    scanf("%d",&k);
+    int matrix[k][3];
+    for ( int i = 0 ; i < k ; i ++){
+        for (int j = 0 ; j < 3 ; j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    printf("Sparse Array (row_index col_index quantity):\n");
+    for ( int i = 0 ; i < k ; i ++){
+        for (int j = 0 ; j < 3 ; j++){
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+        printf("\n");
+    printf("Total quantity ordered per product:\n");
+    int arr[100]={0};
+    for ( int i = 0 ; i < k ; i++){
+        arr[matrix[i][1]] += matrix[i][2];
+    }
+    for ( int i = 0 ; i < 100;i++){
+        if ( arr[i]!=0){
+            printf("Product %d: %d\n",i,arr[i]);
+        }
+    }
+
+}
